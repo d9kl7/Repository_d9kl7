e8bbaa8b95127e54bff9c2ca4c41af0f2ff043c6
daria <glashatarzan@gmail.com>
2025-08-26 12:06:15 +0000

    Налаштував ESLint для Jest

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+// ... інші налаштування
+plugins: [
+    'jest'
+],
+extends: [
+    'airbnb-base',
+    'plugin:jest/recommended'
+],
+env: {
+    'browser': true,
+    'es2021': true,
+    'jest': true
+},
+// ...
