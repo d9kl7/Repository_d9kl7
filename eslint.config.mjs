4c4fd9dff68278cf9cd316de85d11122b43bfd46
daria <glashatarzan@gmail.com>
2025-08-26 12:12:19 +0000

    Додав конфігурацію ESLint для Jest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,23 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
+import jest from "eslint-plugin-jest";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
+  { files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest
+      }
+    },
+    plugins: {
+      js,
+      jest
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...jest.configs.recommended.rules
+    }
+  }
 ]);
